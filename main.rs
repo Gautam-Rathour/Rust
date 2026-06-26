b10dad4634796564437c588e56ec4d98acc8b785
Gautam-Rathour <gau321tam@gmail.com>
2026-06-26 22:30:46 +0530

    Add Practic Code

--- main.rs
+++ main.rs
@@ -1,6 +1,46 @@
-fn main() {
-    let x = 1;
-    x = "RajuRam";
+// fn main() {
 
-    println!("Hello World");
+//     let x: i32 = -50;
+//     let y: u32 = 100;
+//     let z: f32 = 10.05;
+
+//     println!("x: {}", x);
+// }
+
+//=============================================================================
+
+
+// fn main() {
+//     let mut x: i8 = 10;
+
+//     for i in 0..1000 {
+//         x = x + 100;
+//     }
+
+//     println!("x: {}", x);
+// }
+
+//=============================================================================
+
+// fn main() {
+//     let is_male = true;
+//     let is_above_18 = true;
+
+//     if is_male {
+//         println!("You are a male");
+//     } else {
+//         println!("You are not a male");
+//     }
+
+//     if is_male && is_above_18 {
+//         println!("You are a legal adult male");
+//     }
+// }
+
+//=============================================================================
+
+fn main() {
+    let greeting = String::from("Hello World");
+    println!("{}", greeting);
+
 }
