51f536b9329c45d04966098593c759e78fe9a628
Gautam-Rathour <gau321tam@gmail.com>
2026-07-01 21:50:57 +0530

    Add few line for sample

--- main.rs
+++ main.rs
@@ -75,28 +75,44 @@
 //=============================================================================
 
 
+// fn main() {
+
+//     // arrays, maps, strings
+//     let sentence = String::from("my name is harkirat");
+//     let first_word = get_first_word(sentence);
+
+//     let n = 1000;
+//     for i in 0..n {
+//         println!("Hello, world! ()", i);
+//     }
+
+//     print!("First word is: {}", first_word);
+
+// }
+
+// fn get_first_word(sentence: String) -> String {
+//     let mut ans: String = String::from("");
+//     for char in sentence.chars() {
+//         ans.push_str(char.to_string().as_str());
+//         if char == ' ' {
+//             break;
+//         }
+//     }
+//     return ans;
+// }
+
+//=============================================================================
+
+
+
 fn main() {
 
     // arrays, maps, strings
-    let sentence = String::from("my name is harkirat");
-    let first_word = get_first_word(sentence);
-
-    let n = 1000;
-    for i in 0..n {
-        println!("Hello, world! ()", i);
-    }
     
-    print!("First word is: {}", first_word);
-
+    
 }
 
-fn get_first_word(sentence: String) -> String {
-    let mut ans: String = String::from("");
-    for char in sentence.chars() {
-        ans.push_str(char.to_string().as_str());
-        if char == ' ' {
-            break;
-        }
-    }
-    return ans;
-}
+
+
+
+//=============================================================================
